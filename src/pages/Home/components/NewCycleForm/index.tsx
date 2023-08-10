0f635928fccf779ec0bd8938325e96e83ff976f8
Samuel Cintra <128873760+SamuelCintra01@users.noreply.github.com>
2023-08-10 19:16:49 -0300

    feat: :sparkles: Contexto entre rotas

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,7 +1,9 @@
 import { useContext } from 'react'
-import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
-import { CyclesContext } from '../..'
 import { useFormContext } from 'react-hook-form'
+
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+
+import { CyclesContext } from '../../../../contexts/CyclesContext'
 
 export function NewCycleForm() {
   const { activeCycle } = useContext(CyclesContext)
